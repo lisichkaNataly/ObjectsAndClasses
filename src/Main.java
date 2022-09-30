b92393ac4d98ba2eb1afc5b3ed527462c7d0080c
Izmaylova <Natalyatf@yandex.ru>
2022-09-30 18:58:53 +0500

    Домамшнее Задание. Объекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Author authorEn = new Author("Donald","Trump");
+        Author authorRu = new Author("Dmitry","Hara");
+        Book bookEn = new Book("The Way to the Top",2004,authorEn);
+        Book bookRu = new Book("P.Sh.", 2017, authorRu);
+        printBook(bookEn);
+        bookEn.setYear(2022);
+        System.out.println("New book year: " + bookEn.getYear());
 
     }
+
+    private static void printBook(Book book){
+        System.out.println("Book: " + book.getName());
+        System.out.println("Author: " + book.getAuthor().getSurname() + " " + book.getAuthor().getName());
+        System.out.println("Year Publication: " + book.getYear());
+    }
+
 }
